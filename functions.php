5e8992d5d73e37b6b544a0546310cd82bef76786
milesimarco <milesimarco@outlook.com>
2014-09-12 11:25:33 +0200

    0.2.9

--- functions.php
+++ functions.php
@@ -35,7 +35,7 @@ add_theme_support( 'custom-background', $defaults );
 $args = array(
 	'width'         => 1150,
 	'height'        => 110,
-	'default-image' => 'http://www.psdgraphics.com/file/colorful-triangles-background.jpg',
+	'default-image' => ''
 );
 add_theme_support( 'custom-header', $args );
 
