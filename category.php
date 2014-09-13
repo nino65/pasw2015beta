e161dbceef03d18891ae1a298ba3fb3530c693dd
milesimarco <milesimarco@outlook.com>
2014-09-14 00:06:01 +0200

    0.3.11

--- category.php
+++ category.php
@@ -12,9 +12,23 @@ if(function_exists('bcn_display'))
 </div>
 <!-- fine breadcrumbs -->
 	<?php if (have_posts()) : ?>
+
 	<h2 class="posttitle"><?php single_cat_title(); ?></h2>
+
+	<div style="float:right;">
+		<ul>
+			<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+				<option value="">Filtra per mese</option>
+				<?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?>
+			</select>
+			<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+				<option value="">Filtra per anno</option>
+				<?php wp_get_archives('type=yearly&format=option&show_post_count=1'); ?>
+			</select>
+		</ul>
+	</div>
+
 	<h3>Archivio</h3>
-	
 					
 		<?php while (have_posts()) : the_post(); ?>		
 		
